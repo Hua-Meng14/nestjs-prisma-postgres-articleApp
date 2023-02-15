bd80df2b8c4c7b4b0f0acc749504f93810c05874
HuaMeng-14 <lim.huameng19@kit.edu.kh>
2023-02-15 13:43:09 +0700

    feat/crud-nestjs-prisma

---
+++ src/articles/dto/create-article.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+
+export class CreateArticleDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
+    @ApiProperty()
+    title: string
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    @MaxLength(300)
+    @ApiProperty({required:false})
+    description?: string
+
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
+    body: string
+
+    @IsBoolean()
+    @IsOptional()
+    @ApiProperty({required: false, default: false})
+    published?: boolean = false;
+
+}
